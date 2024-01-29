05cb0781d4b80f18e3a98d320ccb6c026418847a
Danielnaz <150403920+Danielnaz@users.noreply.github.com>
2024-01-29 18:17:27 -0500

    commits

---
+++ Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  window.alert("Hello, World!");
+}
+
+var D = "He said, \"You trash kid\", I said, \"Say's you nerd\".";
+window.alert(D);
+
+document.write("\"Be who you are and say what you feel,"
++" because those who mind don't matter and those who matter don't mind.\""
++" -Dr. Seuss");
+
+document.write("<br>");
+
+var B = "Concatenated" +" String"
+document.write(B);
+
+document.write("<br>");
+
+var Him = "Daniel", Her = "Sierra", END = "Natsu", Ghost = "Jin",
+Goat = "Curry";
+document.write(Goat); //Displays a value from the multiple variables
+
+document.write("<br>")
+
+document.write(9 + 10);
+
+document.write("<br>")
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string";
+document.write(Sent1 + Sent2); //concatenate a string by putting both var in the parentheses
